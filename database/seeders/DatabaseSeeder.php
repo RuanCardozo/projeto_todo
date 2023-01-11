ac7cd02993d3c5aeaca03c8d6bc8ab6bc07ac8db
unknown <ruan.ananias@hotmail.com>
2023-01-10 23:53:07 -0300

    criando a factory task

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Category;
+use App\Models\Task;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -17,10 +18,11 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             UserSeeder::class,
-            //TaskSeeder::class
+
         ]);
 
-        Category::factory(5)->create();
+        Category::factory(50)->create();
+        Task::factory(30)->create();
 
 
         // \App\Models\User::factory(10)->create();
