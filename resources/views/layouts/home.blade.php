f07aae8845feaa9f114e87aa19a37c766c475fbf
unknown <ruan.ananias@hotmail.com>
2023-01-22 21:48:03 -0300

    Estrutura Basica de Template

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -11,6 +11,34 @@
     <link rel="stylesheet" href="/assets/css/style.css">
 </head>
 <body>
-a
+    <div class="container">
+
+        <div class="sidebar">
+            Logo
+        </div>
+
+        <div class="content">
+            <nav>
+                <a href="#" class="btn btn-primary">
+                    Criar Tarefa
+                </a>
+            </nav>
+            <main>
+                <section class="graph">
+                    <div className="graph_header"\>
+                        <h2>Progreso do Dia</h2>
+                        <hr class="LinhaHeader"/>
+                        Data
+                    </div>
+                    <div class="graph_header-subtitle"> Tarefas <b>3/6</b> </div>
+                </section>
+                <section class="list">
+                    Lista
+                </section>
+            </main>
+        </div>
+    </div>
+
+
 </body>
 </html>
