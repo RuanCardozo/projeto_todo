4a96cb5ae2f2a535e4b18d4a291c3cc726b83640
ruan <ruan.ananias@hotmail.com>
2023-09-14 13:44:02 -0300

    add animacoes e trasicoes

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -27,14 +27,24 @@
                 <section class="graph">
                     <div class="graph_header"\>
                         <h2>Progreso do Dia</h2>
-                        <hr class="LinhaHeader"/>
-                        Data
+
+                        <div class="graph_header-line">
+                        </div>
+
+                        <div class="graph_header-date">
+                            Data
+                        </div>
                     </div>
+
                     <div class="graph_header-subtitle"> Tarefas <b>3/6</b> </div>
                     <div class="graph-placeholder">
                     </div>
 
-                    <p class="graph_header-task_left">Restam 3 tarefas para serem realizadas</p>
+                    <div class="tasks_left_footer">
+                        <img src="/assets/images/icon-info.png" />
+                        Restam 3 tarefas para serem realizadas
+                    </div>
+
 
                 </section>
                 <section class="list">
@@ -43,6 +53,7 @@
                             <option value="1">Todas as tarefas</option>
                         </select>
                     </div>
+
                     <div class="task_list">
                         <div class="task">
 
@@ -67,24 +78,7 @@
                             </div>
 
                         </div>
-                        <div class="task">
 
-                            <div class="title">
-                                <input type="checkbox" />
-                                <div class="task_title">Titulo da tarefa </div>
-                            </div>
-
-                            <div class="priority">
-                                <div class="sphere">
-                                </div>
-                                <div>Titulo da tarefa </div>
-                            </div>
-
-                            <div class="actions">
-                                   Editar - Excluir
-                            </div>
-
-                        </div>
                     </div>
                 </section>
             </main>
