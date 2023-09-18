3ea50ef5972f6b6446b00f30f7147d56c4f9ee31
ruan <ruan.ananias@hotmail.com>
2023-09-18 13:35:02 -0300

    criando a navegacao

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,15 @@
 <?php
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', [HomeController::class, 'index'])->name('home');
+ROute::get('/task/new', [TaskController::class, 'create'])->name('task.create');
+ROute::get('/task', [TaskController::class, 'create'])->name('task.create');
 
+Route::get('/login', [AuthController::class, 'index'])->name('login');
+Route::get('/register', [AuthController::class, 'register'])->name('register');
 Route::get('/', function () {
     return view('home');
 });
