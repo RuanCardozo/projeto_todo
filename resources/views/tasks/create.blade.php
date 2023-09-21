9e7bb1c0b10bf2b0a19bf4d2dc52b3391ab900ae
ruan <ruan.ananias@hotmail.com>
2023-09-20 22:21:51 -0300

    finalizando tela de inclusao de tasks

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -12,9 +12,33 @@
                 <label for="title">
                     Titulo da task
                 </label>
-                <input name="title" placeholder="Digite o titulo da tarefa" required />
-
+                <input id="title" name="title" placeholder="Digite o titulo da tarefa" required />
             </div>
+
+            <div class="inputArea">
+                <label for="due_date">
+                    Data da realização
+                </label>
+                <input type="date" id="due_date" name="due_date" placeholder="Data de realização" required />
+            </div>
+
+            <div class="inputArea">
+                <label for="category">
+                    Categoria
+                </label>
+                <select id="category" name="category" required>
+                    <option selected disabled value=""> Selecione a categotia </option>
+                    <option>Valor qualqer </option>
+                </select>
+            </div>
+
+            <div class="inputArea">
+                <label for="descri">
+                    Descrição da tarefa
+                </label>
+                <textarea id="descri" name="descripition" placeholder="Digite uma descrição para sua tarefa"></textarea>
+            </div>
+
         </form>
     </section>
 
